2931df37be605564b52f627929e35daff5fc66a7
Angel Angelov <hextwoa@gmail.com>
2016-10-28 20:33:01 +0300

    fix sliding puzzle homework

--- sliding_puzzle/sliding_puzzle.hs
+++ sliding_puzzle/sliding_puzzle.hs
@@ -1,13 +1,14 @@
 import Data.Vector (Vector, fromList, toList, (//), (!), elemIndex)
 import qualified Data.List as List
-import Data.List.Split (chunksOf)
+import Data.List.Split (chunksOf, splitOneOf)
 import Data.Foldable (minimumBy)
 import Data.Maybe (fromJust)
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Debug.Trace
+import Data.Hashable (Hashable, hashWithSalt)
 
 data Node = Node {
                 size  :: Int,        -- length of board side
@@ -15,57 +16,60 @@ data Node = Node {
                 slots :: Vector Int  -- flattened board
             }
 
-main = interact (\x -> "gs: " ++ x)
+main = interact solve
 
-printSolution :: Node -> IO()
-printSolution start = printPath $ search start (goalFor start)
+-- | Extracts a start node from the input string and returns the directions delimited by newlines
+solve :: String -> String
+solve s = showPath $ search start (goalFor start)
+    where start = nodeFromString s
 
+-- | Searches for an optimal path and returns it as a list of nodes
 search :: Node                  -- ^ start
        -> Node                  -- ^ goal
        -> Maybe [Node]          -- ^ shortest path
-search start goal = fmap (reverse . (goal :) . trackPath goal) $ astar openSet closedSet parents fScores gScores goal
+search start goal = fmap (reverse . (goal :) . trackPath goal) $ astar openSet closedSet parents endScores startScores goal
     where
         openSet = Set.insert start Set.empty
         closedSet = Set.empty
-        parents = Map.empty
-        gScores = Map.insert start 0 Map.empty
-        fScores = Map.insert start (h start goal) Map.empty
+        parents = HashMap.empty
+        startScores = HashMap.insert start 0 HashMap.empty
+        endScores = HashMap.insert start (heuristic start goal) HashMap.empty
 
 
-astar :: Set Node               -- ^ open set
-      -> Set Node               -- ^ closed set
-      -> Map Node Node          -- ^ parents
-      -> Map Node Int           -- ^ fScores (cost of paths to the goal which go through each node)
-      -> Map Node Int           -- ^ gScores (cost of path from start to each node)
-      -> Node                   -- ^ goal
+astar :: Set Node                   -- ^ open set
+      -> Set Node                   -- ^ closed set
+      -> HashMap Node Node          -- ^ parents
+      -> HashMap Node Int           -- ^ endScores (cost of paths to the goal which go through each node)
+      -> HashMap Node Int           -- ^ startScores (cost of path from start to each node)
+      -> Node                       -- ^ goal
 
-      -> Maybe (Map Node Node)  -- ^ final parents
+      -> Maybe (HashMap Node Node)  -- ^ final parents
 
-astar openSet closedSet parents fScores gScores goal
-    | current == goal           = Just parents
+astar openSet closedSet parents endScores startScores goal
     | (Set.size openSet) == 0   = Nothing
-    | otherwise                 = astar openSet' closedSet' parents' fScores' gScores' goal
+    | current == goal           = Just parents
+    | otherwise                 = astar openSet' closedSet' parents' endScores' startScores' goal
     where
-        parents' = foldr (\x -> Map.insert x current) parents usefulNeighbours
-        gScores' = foldr (\x -> Map.insert x tScore) gScores usefulNeighbours
-        fScores' = foldr (\x -> Map.insert x (tScore + (h x goal))) fScores usefulNeighbours
+        parents' = foldr (\x -> HashMap.insert x current) parents usefulNeighbours
+        startScores' = foldr (\x -> HashMap.insert x tScore) startScores usefulNeighbours
+        endScores' = foldr (\x -> HashMap.insert x (tScore + (heuristic x goal))) endScores usefulNeighbours
         openSet' = Set.delete current (Set.union openSet (Set.fromList newNeighbours))
         closedSet' = Set.insert current closedSet
-        tScore = (Map.findWithDefault inf current gScores) + 1
+        tScore = (HashMap.lookupDefault inf current startScores) + 1
         usefulNeighbours = closerNeighbours ++ newNeighbours
-        closerNeighbours = filter (\x -> ((Set.member x openSet) && (tScore < (Map.findWithDefault inf x gScores)))) neighbours
+        closerNeighbours = filter (\x -> ((Set.member x openSet) && (tScore < (HashMap.lookupDefault inf x startScores)))) neighbours
         newNeighbours = filter (\x -> Set.notMember x openSet) neighbours
         neighbours = filter (\x -> Set.notMember x closedSet) (children current)
-        current = minimumBy (\x y -> compare (fScores Map.! x) (fScores Map.! y)) openSet
+        current = minimumBy (\x y -> compare (endScores HashMap.! x) (endScores HashMap.! y)) openSet
 
 
-trackPath :: Node -> Map Node Node -> [Node]
+trackPath :: Node -> HashMap Node Node -> [Node]
 trackPath goal parents
 
     | Just parent   <- lookup = parent : (trackPath parent parents)
     | Nothing       <- lookup = []
     where
-        lookup = Map.lookup goal parents
+        lookup = HashMap.lookup goal parents
 
 
 children :: Node -> [Node]
@@ -80,21 +84,24 @@ children Node { size=s, zero=z, slots=b } = moveUp ++ moveDown ++ moveLeft ++ mo
             | otherwise = [Node { size=s, zero=z + s, slots=(b // [(z, b ! (z + s))]) }]
 
         moveLeft
-            | z `mod` (length b) == 0 = []
+            | z `mod` s == 0 = []
             | otherwise = [Node { size=s, zero=z - 1, slots=(b // [(z, b ! (z - 1))]) }]
 
         moveRight
-            | z `mod` (length b) == length b - 1 = []
+            | z `mod` s == s - 1 = []
             | otherwise = [Node { size=s, zero=z + 1, slots=(b // [(z, b ! (z + 1))]) }]
 
 
 
+-- | manhattan distance between two nodes
 nodeDist :: Node -> Node -> Int
 nodeDist a b = boardDist (size a) (slotsWithZero a) (slotsWithZero b)
 
+-- | manhattan distance between two boards
 boardDist :: Int -> (Vector Int) -> (Vector Int) -> Int
 boardDist len a b = sum $ map (\i -> indexDist len i (fromJust $ elemIndex (a ! i) b)) [0..(length a - 1)]
 
+-- | manhattan distance between coordinates
 indexDist :: Integral a => a -> a -> a -> a
 indexDist len a b = (abs (xa - xb)) + (abs (ya - yb))
     where
@@ -103,10 +110,11 @@ indexDist len a b = (abs (xa - xb)) + (abs (ya - yb))
         ya = a `div` len
         yb = b `div` len
 
-
+-- | get the goal node with the same size as the given node
 goalFor :: Node -> Node
 goalFor node = makeNode $ [1..((size node)^2 - 1)] ++ [0]
 
+-- | make a node from a list of integers. 
 makeNode :: [Int] -> Node
 makeNode plates = Node {
     size = truncate $ sqrt $ fromIntegral $ length plates,
@@ -114,21 +122,25 @@ makeNode plates = Node {
     slots = fromList plates
 }
 
-printNode :: Node -> IO ()
-printNode = putStrLn . show
 
-printPath :: Maybe [Node] -> IO()
-printPath (Just nodes)  = mapM_ putStrLn $ pathDirections nodes
-printPath Nothing       = putStrLn "no solution"
+showPath :: Maybe [Node] -> String
+showPath (Just nodes) = (List.intercalate "\n" $ pathDirections nodes) ++ "\n"
+showPath Nothing = "no solution\n"
 
 instance Show Node where
-    show node = "{" ++ (List.intercalate " . " $ map (List.intercalate " ") $ map (map show) $ matrix node) ++ "}"
+    show node = (List.intercalate "\n" $ map (List.intercalate " ") $ map (map show) $ matrix node) ++ "\n"
 
 instance Eq Node where
     (==) a b = ((size a) == (size b)) && ((slotsWithZero a) == (slotsWithZero b))
 
 instance Ord Node where
     compare a b = compare (slotsWithZero a) (slotsWithZero b)
+
+instance Hashable Node where
+    hashWithSalt salt node = hashWithSalt salt (toList $ slotsWithZero node)
+
+nodeFromString :: String -> Node
+nodeFromString s = makeNode $ map read $ tail $ filter (not . null) $ splitOneOf "\n\r " s
 
 matrix :: Node -> [[Int]]
 matrix node = chunksOf (size node) $ toList $ slotsWithZero node
@@ -147,8 +159,8 @@ direction Node { size=s, zero=z1 } Node { zero=z2 }
 pathDirections :: [Node] -> [String]
 pathDirections nodes = zipWith direction (init nodes) (tail nodes)
 
-h :: Node -> Node -> Int
-h = nodeDist
+heuristic :: Node -> Node -> Int
+heuristic = nodeDist
 
 inf :: Int
 inf = maxBound
