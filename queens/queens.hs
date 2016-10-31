bf8629f774d0789e2506a95ae27712d3cafdf370
Angel Angelov <hextwoa@gmail.com>
2016-10-31 20:39:24 +0200

    document code a bit

--- queens/queens.hs
+++ queens/queens.hs
@@ -32,27 +32,35 @@ optimiseQueen (Board board) queen = randMinBy conflicts $ map moveAt [0..(length
     where
         moveAt pos = Board (board // [(queen, pos)])
 
+-- | counts the number of conflicting queens on the board
 conflicts :: Board -> Int
 conflicts board = countTrues $ map (uncurry conflict) $ queenPairs board
 
+-- | all possible queen pairs on the board
 queenPairs :: Board -> [(Point, Point)]
 queenPairs (Board board) = map points $ pairsUnder $ Vec.length board
     where
         points (x1, x2) = ((point x1), (point x2))
         point x = Point x (board ! x)
 
+-- | all pairs (i, j) for 0 <= i < j < n
 pairsUnder :: Int -> [(Int, Int)]
 pairsUnder n = [(x, y) | (x:ys) <- tails [0..n - 1], y <- ys]
 
+-- | random board with side N
 initialBoard :: Int -> IO Board
 initialBoard n = makeBoard <$> (replicateM n $ randomRIO (0, n - 1))
 
+-- | check if two queens at the given points are at a conflict
 conflict :: Point -> Point -> Bool
-conflict a b = (xyconflict a b) || (diagconflict a b)
+conflict a b = (yconflict a b) || (diagconflict a b)
+-- no chance of x conflict because we have 1 queen per row
 
-xyconflict :: Point -> Point -> Bool
-xyconflict (Point x1 y1) (Point x2 y2) = x1 == x2 || y1 == y2
+-- | check if two queens are aligned vertically
+yconflict :: Point -> Point -> Bool
+yconflict (Point _ y1) (Point _ y2) = y1 == y2
 
+-- | check if two queens are aligned diagonally
 diagconflict :: Point -> Point -> Bool
 diagconflict (Point x1 y1) (Point x2 y2) = (abs $ x1 - x2) == (abs $ y1 - y2)
 
@@ -62,9 +70,11 @@ instance Show Board where
 instance Show Point where
     show (Point x y) = "[" ++ (show x) ++ ", " ++ (show y) ++ "]"
 
+-- | board side size
 size :: Board -> Int
 size (Board queens) = Vec.length queens
 
+-- | make a board from a list of queen positions on their rows
 makeBoard :: [Int] -> Board
 makeBoard queens = Board $ Vec.fromList queens
 
